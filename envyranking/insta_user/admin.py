ac9447d1b7ed05131bf0fe0dede4c4db1573ee33
Seabyeok-Shin <toqur1219@naver.com>
2020-08-04 01:53:32 +0900

    feat:adding auto updating task

--- envyranking/insta_user/admin.py
+++ envyranking/insta_user/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(just_insta_id)
 admin.site.register(insta_user_Data)
--- envyranking/insta_user/models.py
+++ envyranking/insta_user/models.py
@@ -1,21 +1,12 @@
 from django.db import models
 from igramscraper.instagram import Instagram
 
-
-class just_insta_id(models.Model):
-    username = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.username 
-
-
 class insta_user_Data(models.Model):
     id_number = models.BigIntegerField(blank=True)
     username = models.CharField(max_length=100)
-    #username = models.OneToOneField(just_insta_id, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=100, blank=True)
     biograghy = models.CharField(max_length=500, blank=True, null=True)
-    profilepic_url = models.URLField(blank=True)
+    profilepic_url = models.URLField(max_length=500, blank=True)
     external_url = models.URLField(blank=True, null=True)
     number_published = models.BigIntegerField(blank=True)
     number_followers = models.BigIntegerField(blank=True)
@@ -28,7 +19,7 @@ class insta_user_Data(models.Model):
         account = instagram.get_account(self.username)
         self.id_number = account.identifier
         self.fullname = account.full_name
-        self.biograghy = account.biography.encode('utf-8')
+        self.biograghy = account.biography
         self.profilepic_url = account.get_profile_picture_url()
         self.external_url = account.external_url
         self.number_published = account.media_count
@@ -37,7 +28,6 @@ class insta_user_Data(models.Model):
         self.is_private = account.is_private
         self.is_verified = account.is_verified
         super().save(*args, **kwargs)
-
-
+    
     def __str__(self):
         return str(self.username)
