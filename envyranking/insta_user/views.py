d57fdc65ecc7c83a705c47f1f6d236d8e944d355
Seabyeok-Shin <toqur1219@naver.com>
2020-08-16 03:38:30 +0900

    feat:adding 404 fun

--- envyranking/insta_user/views.py
+++ envyranking/insta_user/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.http import HttpResponse, Http404
 from .models import insta_user_Data
 
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.db.models import Q, Count
 
 from .forms import PostForm
+from igramscraper.instagram import Instagram
+
+import requests
 
 
 def index(request):
         userdata = insta_user_Data.objects.order_by('-number_followers')[0:99]
         context = {
                 'userdata': userdata,
+                
                 }
-        
         if request.method =="POST":
             username = request.POST["username"]
-            if insta_user_Data.objects.filter(username=username).exists()==False:
+            if insta_user_Data.objects.filter(username=username).exists()==False and request.status_code == not 404:
                 new_insta_user = insta_user_Data()
                 new_insta_user.username = username
                 new_insta_user.save()
-
+            else :
+                print('errore')
+            
 
 
         return render(request, "index.html", context)
