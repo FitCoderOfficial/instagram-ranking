672b4f6073afadd38d1cc6a325f1916ca17bfd2b
Seabyeok-Shin <toqur1219@naver.com>
2020-08-19 04:04:23 +0900

    feat:scraping tiktok follower

--- envyranking/tiktok_user/test.py
+++ envyranking/tiktok_user/test.py
@@ -1,5 +1,15 @@
 from TikTokApi import TikTokApi
 api = TikTokApi()
 
-print(api.getUser('woori88'))
-print(api.getUserObject('just_grace'))
+#print(api.getUser('woori88')) 
+
+scraper_list = api.getUserObject('just_grace')
+ 
+scraper_list2 = api.getUser('just_grace')
+
+
+for e in scraper_list2:
+    print(e)
+
+print(scraper_list2['userInfo']['stats'])
+
